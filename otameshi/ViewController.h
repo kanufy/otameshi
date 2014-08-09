9430a585d55d8c3c561e5d7fae0ee1ea614b72ce
kanae <kanae.h.1102@gmail.com>
2014-08-09 13:14:20 +0900

    Initial Commit

---
+++ otameshi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  otameshi
+//
+//  Created by hata kanae on 2014/08/09.
+//  Copyright (c) 2014年 hata kanae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ otameshi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  otameshi
+//
+//  Created by hata kanae on 2014/08/09.
+//  Copyright (c) 2014年 hata kanae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
